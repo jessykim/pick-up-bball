131d5195b2d37f498e064ffe190c5c15f89c1b44
jessikim <jessica.kim17@gmail.com>
2022-09-30 14:03:35 -0500

    create pay schema in models

---
+++ models/pay.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const paySchema = new Schema({
+  player: {
+    type: Schema.Types.ObjectId, 
+    ref: "Player"
+  },
+  received: Boolean,
+  paid: Boolean,
+}, {
+  timestamps: true,
+})
+
+const User = mongoose.model('Pay', paySchema)
+
+export {
+  Pay
+}
